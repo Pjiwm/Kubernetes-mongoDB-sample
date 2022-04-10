c2fd935cf8f6805e6cee9a42704cb7ad9c97d31d
Pjiwm <pim.munne@gmail.com>
2022-04-11 00:40:39 +0200

    first commit

---
+++ node-app/app.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const express = require('express')
+
+const app = express()
+
+mongoose.Promise = global.Promise
+// const connectionString = process.env.DATABASE_CONNECTION
+const connectionString = "mongodb://admin-user:secret@127.0.0.1:27017"
+mongoose.connect(`${connectionString}/appDB&directConnection=true&serverSelectionTimeoutMS=2000"`).catch((err) => {
+    console.log(err)
+})
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().catch((err) => console.log(err))
+
+// DATABASE_CONNECTION=mongodb://admin:admin@172.27.192.1:27017/admin?directConnection=true&serverSelectionTimeoutMS=2000
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`app listening on port ${port}!`)
+})
